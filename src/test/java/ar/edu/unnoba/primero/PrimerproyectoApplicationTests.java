5b0a60900f2279ba6bc3ff4b661113def3bc6f52
JohnnyEtche <johnnyetchepare@gmail.com>
2020-11-02 18:26:56 -0300

    Commit inicial

---
+++ src/test/java/ar/edu/unnoba/primero/PrimerproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.primero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
