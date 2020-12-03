f1af48178ef0f5a362feba957515c64f4c5ec69e
Chris <christian_maccarino@outlook.com>
2020-12-03 17:59:36 -0300

    Modificación de página de inicio y modificación de ConfirmBookingRequestDTO

--- src/main/java/ar/edu/unnoba/primero/DTO/ConfirmBookingRequestDTO.java
+++ src/main/java/ar/edu/unnoba/primero/DTO/ConfirmBookingRequestDTO.java
@@ -1,4 +1,48 @@
 package ar.edu.unnoba.primero.DTO;
 
+import java.text.SimpleDateFormat;
+
 public class ConfirmBookingRequestDTO {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private long roomId;
+    private String CheckIn;
+    private String CheckOut;
+    private int occupancy;
+
+    public static SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
+
+    public long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getCheckIn() {
+        return CheckIn;
+    }
+
+    public void setCheckIn(String checkIn) {
+        CheckIn = checkIn;
+    }
+
+    public String getCheckOut() {
+        return CheckOut;
+    }
+
+    public void setCheckOut(String checkOut) {
+        CheckOut = checkOut;
+    }
+
+    public int getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
+
 }
